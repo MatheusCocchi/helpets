60673e36181196fb4da8371bb3592e1005432b37
DanielTomaz <daniel.altomaz@gmail.com>
2020-05-06 15:57:15 -0300

    Tela cadastro de animal em Desenvolvimento

--- lib/view/cadastroAnimal_widget.dart
+++ lib/view/cadastroAnimal_widget.dart
@@ -26,7 +26,82 @@ class _CadastroAnimalWidgetState extends State<CadastroAnimalWidget> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-
+            Container(
+              height: MediaQuery.of(context).size.height /2,
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.only(top: 32),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width / 1.2,
+                    height: 50,
+                    padding:
+                        EdgeInsets.only(top: 4, left: 16, right: 16, bottom: 4),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(color: Colors.black12, blurRadius: 5)
+                        ]),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          // icon: Icon(Icons.email,
+                          //color: Colors.grey
+                          //),
+                          border: InputBorder.none,
+                          hintText: 'Nome'),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 1.2,
+                    height: 50,
+                    margin: EdgeInsets.only(top: 20),
+                    padding:
+                        EdgeInsets.only(top: 4, left: 16, right: 16, bottom: 4),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(color: Colors.black12, blurRadius: 5)
+                        ]),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          // icon: Icon(Icons.vpn_key,
+                          //color: Colors.grey
+                          //),
+                          border: InputBorder.none,
+                          hintText: 'Raça'),
+                    ),
+                  ),
+                  Spacer(),
+                  
+                  Container(
+                    width: MediaQuery.of(context).size.width / 1.2,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color(0xFFC71585),
+                          Color(0xFFFF1493)
+                        ],
+                      ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(50)
+                        ),
+                    ),
+                      child: FlatButton(
+                      child: Text('Salvar'.toUpperCase(),
+                        style: TextStyle(
+                          color: Colors.white, 
+                          fontWeight: FontWeight.bold  
+                        ),
+                      ),
+                      onPressed: () {}
+                    ),  
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
